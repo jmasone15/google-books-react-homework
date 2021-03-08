5bb160cb0cd2f40538036e66c1929109a62fafc8
Jordan Masone <jordanmasone1@gmail.com>
2021-03-08 13:42:00 -0500

    Deployment.

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://guarded-brook-67536.herokuapp.com"],
     credentials: true,
 }));
 
